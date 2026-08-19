4f5df98bab94fa0435fc1d041e21ce7eeb7b951f
Aeron C <aerongcanlas@gmail.com>
2026-08-18 20:33:36 -0700

    add sidebar

--- apps/web/src/components/ui/index.ts
+++ apps/web/src/components/ui/index.ts
@@ -1,12 +1,15 @@
-export { Box } from './Box';
-export { BoxColumn } from './BoxColumn';
-export { BoxRow } from './BoxRow';
-export { Button } from './Button';
-export { Input } from './Input';
+export { Box } from "./Box";
+export { BoxColumn } from "./BoxColumn";
+export { BoxRow } from "./BoxRow";
+export { Button } from "./Button";
+export { Card } from "./Card";
+export { Empty } from "./Empty";
+export { Input } from "./Input";
 export {
     ResizableHandle,
     ResizablePanel,
     ResizablePanelGroup,
-} from './Resizable';
-export { Separator } from './Separator';
-export { TextBox } from './TextBox';
+} from "./Resizable";
+export { Separator } from "./Separator";
+export { TextBox } from "./TextBox";
+export { Tooltip } from "./Tooltip";
